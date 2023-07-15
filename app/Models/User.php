24abfee605f29ba41eaf16b9650ab84831816241
haejin <haejin.yang91@gmail.com>
2023-07-15 22:35:09 +0900

    fix: 마이그레이션 과정에서 일어난 타입버그 수정

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
