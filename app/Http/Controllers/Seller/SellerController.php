349d75c921c06adc03c7b12a604b1497ee15ae7d
haejin <haejin.yang91@gmail.com>
2023-07-17 15:47:36 +0900

    feat: SellerController 구현

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Seller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SellerController extends Controller
 {
@@ -12,7 +14,9 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
+        $sellers = Seller::has('products')->get();
+
+        return response()->json(['data' => $sellers], Response::HTTP_OK);
     }
 
     /**
@@ -20,6 +24,8 @@ class SellerController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $seller = Seller::has('products')->findOrFail($id);
+
+        return response()->json(['data' => $seller], Response::HTTP_OK);
     }
 }
