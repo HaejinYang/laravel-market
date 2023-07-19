9a89a4e2b9a608b9aa6d87857f16a761f6562394
haejin.yang91 <haejin.yang91@gmail.com>
2023-07-19 13:08:35 +0900

    feat: ModelNotFoundException 핸들링

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,14 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
+use Illuminate\Support\Arr;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -30,9 +35,23 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
 
-        $this->renderable(function (ValidationException $e) {
+    public function render($request, Throwable $e)
+    {
+        /*
+         * ModelNotFoundException은 기본적으로 $internalDontReport에 있지만
+         * render 함수를 오버라이딩하여 처리할 수 있다.
+         */
+        if($e instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($e->getModel()));
+            return $this->errorResponse("{$model}가 존재하지 않습니다.", Response::HTTP_NOT_FOUND);
+        }
+
+        if($e instanceof ValidationException) {
             return $this->errorResponse($e->validator->errors()->getMessages(),Response::HTTP_UNPROCESSABLE_ENTITY);
-        });
+        }
+
+        return parent::render($request, $e);
     }
 }
