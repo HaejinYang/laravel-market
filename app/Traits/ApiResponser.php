6ccaa47d9b1549a466870786711dc7913a52b53c
haejin <haejin.yang91@gmail.com>
2023-07-22 20:32:28 +0900

    feat: 동적 페이지 카운트 추가

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Response;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -83,8 +84,17 @@ trait ApiResponser
 
     protected function paginate(Collection $collection)
     {
+        $rules = [
+            'per_page' => ['integer', 'min:2', 'max:50'],
+        ];
+
+        Validator::validate(request()->all(), $rules);
         $page = LengthAwarePaginator::resolveCurrentPage();
         $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = request()->per_page;
+        }
+
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 
         $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
